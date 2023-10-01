578b1f884a453c9f4dd4a1fbcb0aca1553397018
Alan <90253861+alannn123@users.noreply.github.com>
2023-10-01 11:21:23 +0530

    Update +page.svelte

---
+++ src/routes/(tools)/online-spreadsheet/+page.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	import { Button, GradientButton } from 'flowbite-svelte';
+	import Intro from '$lib/Intro.svelte';
+	import { Label, Input, Range, Radio, Checkbox } from 'flowbite-svelte';
+	import { onMount } from 'svelte';
+	
+	export let data;
+
+	// Might need these for tabular view
+	let innerWidth: any = null; // defining this solve the of svelte:window tag  
+	let innerHeight: any = null; // and now all other elements are visible
+	let screenWidth: any = null;
+	let screenHeight: any = null;
+	
+	
+
+</script>
+
+<Intro heading={data.meta.title} description={data.meta.description} />
+
+<svelte:window bind:innerWidth bind:innerHeight />
+
+<section class="bg-white dark:bg-gray-900">
+	<!--
+
+		<div class="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
+			<div class="space-y-8 md:grid md:grid-cols-2 lg:grid-cols-4 md:gap-12 md:space-y-0">
+				//Code goes here
+			</div>
+		</div>
+	-->
+	<div class="flex justify-center items-center">
+		<Button size="xl" outline color="blue" class="m-6">Open New Spreadsheet<br/>(.csv)</Button>
+	    <GradientButton size="xl" color="blue" class="m-6">Create New Spreadsheet<br/>(.csv)</GradientButton>
+	</div>
+</section>
